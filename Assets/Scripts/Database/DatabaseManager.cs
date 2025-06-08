a8b9dad2660a224d03fd959af7b6735bfec303f0
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-06-08 04:25:54 -0300

    Cargar Users en Firebase

--- Assets/Scripts/Database/DatabaseManager.cs
+++ Assets/Scripts/Database/DatabaseManager.cs
@@ -3,25 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Database;
 using UnityEngine.UI;
+using Google.MiniJSON;
+using System;
 
 public class DatabaseManager : MonoBehaviour
 {
 
-    public InputField Nombre;
-    public InputField Puntaje;
+    public string nombre;
+    public int puntaje;
 
+    [SerializeField] Creature playerCreature;
     private string userID;
+    const string databaseUrl = "https://console.firebase.google.com/u/0/project/trabajointegradorfb/database/trabajointegradorfb-default-rtdb/data/~2F?hl=es-419";
     // Start is called before the first frame update
-        
+
+    private DatabaseReference reference;
+
     void Start()
     {
+        
         userID = SystemInfo.deviceUniqueIdentifier;
-        DatabaseReference reference= FirebaseDatabase.DefaultInstance.RootReference;
+        reference= FirebaseDatabase.DefaultInstance.RootReference;
+        //reference = FirebaseDatabase.GetInstance(databaseUrl).RootReference;
     }
 
     public void CreateUser()
     {
-        //User newUser= new User(Nombre.);
+        User newUser= new User("TestUser", puntaje);
+        string json=JsonUtility.ToJson(newUser);
+        reference.Child("Usuarios").SetRawJsonValueAsync(json);
+        Debug.Log("User creado");
+
+    }
+
+    public IEnumerator GetNombre(Action<String> oncallback)
+    {
+        var userData = reference.Child("Usuarios").Child(userID).Child("nombre").GetValueAsync();
+        yield return new WaitUntil(predicate:()=>userData.IsCompleted);
+        if (userData!=null)
+        {
+            DataSnapshot snapshot=userData.Result;
+            oncallback.Invoke(snapshot.Value.ToString());
+            Debug.Log(snapshot.Value.ToString());
+        }
+    }
+
+    private void Update()
+    {
+         puntaje= playerCreature.score;
+    }
+
+
+    public void LoadUsuario()
+    {
+        StartCoroutine(GetNombre((String nombre) =>
+        {
+            Debug.Log( $"Usuario: {nombre}");
+        }));
     }
 
 }
